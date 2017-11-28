fcd9916daeb7166b8fd26afd62719998ce5f017f
dedy <dedyibrahym23@gmail.com>
2017-11-29 00:24:26 +0700

    perubahan

--- application/views/V_login.php
+++ application/views/V_login.php
@@ -1,7 +1,7 @@
 <html><head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Sisco</title>
+  <title>BKIPM</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -18,7 +18,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="<?php echo base_url(); ?>"><b>Sisco</b><br>System Company</a>
+    <a href="<?php echo base_url(); ?>"><b>BKIPM</b><br>MAMUJU</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
--- application/views/umum/V_header.php
+++ application/views/umum/V_header.php
@@ -15,7 +15,7 @@ if($level == 'admin' || $level == 'user')
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Sisco</title>
+  <title>BKIPM</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
