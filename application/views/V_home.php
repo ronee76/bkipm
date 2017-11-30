e473876cccb934e5daf65424a5e032a957e2e7ad
dedy <dedyibrahym23@gmail.com>
2017-11-30 23:27:48 +0700

    homes

--- application/views/V_home.php
+++ application/views/V_home.php
@@ -3,27 +3,21 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Meteor HTML CSS Template</title>
-<!--
+        <title>BKIPM</title>
 
-Template 2089 Meteor
-
-http://www.tooplate.com/view/2089-meteor
-
--->
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/fontAwesome.css">
-        <link rel="stylesheet" href="css/hero-slider.css">
-        <link rel="stylesheet" href="css/tooplate-style.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>css/fontAwesome.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>css/hero-slider.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>css/tooplate-style.css">
 
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
 
-        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <script src="<?php echo base_url('assets/'); ?>js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
 
 <body>
